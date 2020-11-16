56ec1cd6c188fd9158b189275c18e0e5da15ae5a
unknown <afnanmahmood52@gmail.com>
2020-11-16 18:22:08 +0500

    update frontend

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import Typography from '@material-ui/core/Typography';
+import 'fontsource-roboto';
+import { Divider, Grid } from '@material-ui/core';
+import classes from './App.module.css'
+import Transaction from './component/Transaction';
+
 
 function App() {
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Grid container xs ={12} justify='center' direction='column' alignItems='center' style={{marginTop:'50px'}} >
+      <Grid item xs={10} md={6} >
+
+            <Typography variant='h3' style={{textAlign:'center', fontWeight:'600', color:'green', marginBottom:'20px'}}>Expense Tracker</Typography>
+
+            <div className={classes.balance_section}>
+              <Typography variant='h5' >Your Balance</Typography>
+              <Typography variant='h5' style={{fontWeight:'600', color:'green'}}>2600.00 PKR</Typography>
+            </div>
+
+            <div className={classes.detail_expense}>
+              
+              <div className={classes.income_section} >
+                <Typography variant='h6' style={{fontWeight:'600', color:'blue'}} >INCOME</Typography>
+                <Typography variant='h6' style={{fontWeight:'500', color:'blue'}} >30000 PKR</Typography>
+              </div>
+            
+              <hr className={classes.divider}/>
+              <div className={classes.expense_section}>
+                <Typography variant='h6' style={{fontWeight:'600', color:'red'}} >EXPENSE</Typography>
+                <Typography variant='h6' style={{fontWeight:'500', color:'red'}}>30000 PKR</Typography>
+              </div>
+            </div>
+
+            <Transaction/>        
+          
+      
+      </Grid>
+    </Grid>    
   );
 }
 
---
+++ src/component/TranscItem.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import classes from './TranscItem.module.css'
+import EditIcon from '@material-ui/icons/Edit';
+import DeleteForeverIcon from '@material-ui/icons/DeleteForever';
+import IconButton from '@material-ui/core/IconButton';
+
+export default function TranscItem(props) {
+    
+    const border_color = props.value[0] === '+' ? 'blue':'red'
+  return (
+    <div className={classes.trans_item} style={{borderRight: `6px solid ${border_color}`}}>
+      <h6 className={classes.trans_title}>{props.title}</h6>
+
+      <div className={classes.rightSection}>
+
+        <h6 className={classes.trans_value}>{props.value}</h6>
+        <div className={classes.editButton}>
+            <IconButton size="small">
+                <EditIcon size="small"/>
+            </IconButton>
+
+            <IconButton size="small">
+                <DeleteForeverIcon size="small"/>
+            </IconButton>
+        </div>
+      </div>
+
+    </div>
+  )
+}
